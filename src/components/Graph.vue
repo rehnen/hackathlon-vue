aba1b0f0099850c4e7a0aadd95d52140720ee859
emarreh <marcus.rehn@ericsson.com>
2018-04-14 16:00:35 +0200

    Added d3

---
+++ src/components/Graph.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="graph">
+    </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+
+export default {
+  name: 'Graph',
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    const items = [
+      { text: 1 },
+      { text: 2 },
+      { text: 3 },
+      { text: 4 },
+    ];
+    const p = d3.select('body')
+      .selectAll('p');
+
+    p.data(items)
+      .enter()
+      .append('p')
+      .merge(p)
+      .text(d => d.text);
+    console.log(d3.selectAll('p'));
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="splash-screen">
         <h1>Test</h1>
+        <graph></graph>
     </div>
 </template>
 
 <script>
+import Graph from '@/components/Graph.vue';
+
 export default {
   name: 'splash-screen',
+  components: {
+    Graph,
+  },
   created() {
     this.$http.get('https://api.github.com/users/rehnen/repos')
-      .then((result) => {
-        console.log(result);
-      });
+      .then(console.log);
   },
 };
 </script>
