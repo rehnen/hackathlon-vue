7322fbcf3d9d72a45c6ac5d5bbaafc4bf5ab6594
Leon <leon.fischer@ericsson.com>
2018-04-14 14:08:48 +0200

    App layout first

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="top">
+            <span id="title">Plan 2 - Bath app</span>
+        </div>
+        <div id="main">
+            <router-view/>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    body, html {
+        padding: 0;
+        margin: 0;
     }
-  }
-}
+    #top{
+        display: inline-block;
+        background-color:#F0FBFF;
+        height: 50px;
+        width: 100%;
+        border-bottom: #BDF5FF 1px solid;
+    }
+    #title{
+        display: inline-block;
+        margin-left: 2.4rem;
+        padding-top: 15px;
+        color: #1B2F33;
+        font-weight: bold;
+
+    }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        display: block;
+        margin-top: 0;
+        width: 100%;
+    }
 </style>
